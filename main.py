72b36c1df75610c0b03e3907d60f1ae4c20fbc3a
Klaudia Musioł <156118884+KlaudiaMusiol@users.noreply.github.com>
2024-02-11 17:53:43 +0100

    Turtle Race

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 from turtle import Turtle, Screen
+import random
 
-franek = Turtle()
 screen = Screen()
+screen.setup(500, 400)
+franek = Turtle(shape="turtle")
+colors = ("red", "orange", "coral", "pink", "purple", "violet", "deeppink")
 
 
-def move_forward():
-    franek.forward(10)
+def start_position(one_turtle, turtle_number):
+    one_turtle.penup()
+    one_turtle.goto(x=-230, y=-200 + turtle_number * 50)
 
 
-def move_backward():
-    franek.backward(10)
+is_race_on = False
 
+turtles = []
+for color in colors:
+    color = Turtle(shape="turtle")
+    turtles.append(color)
 
-def counter_clockwise():
-    franek.left(10)
+no_of_turtles = 0
+for turtle in turtles:
+    no_of_turtles += 1
+    turtle.color(colors[no_of_turtles - 1])
+    start_position(turtle, no_of_turtles)
 
+user_bet = screen.textinput("RACE", "Which turtle will win the race? Please enter the color:").lower()
 
-def clockwise():
-    franek.right(10)
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
 
-def clear_back():
-    franek.clear()
-    franek.up()
-    franek.home()
-    franek.down()
+    for turtle in turtles:
+        if turtle.xcor() >= 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner.")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner.")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
 
-
-screen.listen()
-screen.onkey(key="w", fun=move_forward)
-screen.onkey(key="s", fun=move_backward)
-screen.onkey(key="a", fun=counter_clockwise)
-screen.onkey(key="d", fun=clockwise)
-screen.onkey(key="c", fun=clear_back)
 
 screen.exitonclick()
